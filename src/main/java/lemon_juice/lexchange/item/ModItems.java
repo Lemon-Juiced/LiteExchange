9a93d3f91375936d3a4c22a1aaa179d13309eeab
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-01-02 20:24:36 -0500

    Added Balance Sword

--- src/main/java/lemon_juice/lexchange/item/ModItems.java
+++ src/main/java/lemon_juice/lexchange/item/ModItems.java
@@ -1,7 +1,9 @@
 package lemon_juice.lexchange.item;
 
 import lemon_juice.lexchange.Lexchange;
+import lemon_juice.lexchange.item.tools.ModTiers;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,6 +14,7 @@ public class ModItems {
 
     public static final RegistryObject<Item> BALANCE_SHARD = ITEMS.register("balance_shard", () -> new BalanceShardItem(new Item.Properties()));
     public static final RegistryObject<Item> BALANCE_STONE = ITEMS.register("balance_stone", () -> new BalanceStoneItem(new Item.Properties()));
+    public static final RegistryObject<Item> BALANCE_SWORD = ITEMS.register("balance_sword", () -> new SwordItem(ModTiers.BALANCE, 1, -2.4f, new Item.Properties()));
     public static final RegistryObject<Item> INERT_STONE = ITEMS.register("inert_stone", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus){
